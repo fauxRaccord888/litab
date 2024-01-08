d929dab6dd591de91b04c63dc31e9bc63425e21d
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-01-08 21:42:43 +0900

    feat: Carousel 컴포넌트 추가

---
+++ src/lib/components/common/Carousel.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { PropsWithChildrenNode } from "$lib/types/components";
+
+export default function Carousel(props: PropsWithChildrenNode) {
+    const [selected, setSelected] = useState(0)
+    const { children } = props
+
+    const handleClickPrevious = () => {
+        const newSelected = selected-1
+        setSelected(newSelected)
+    }
+
+    const handleClickNext = () => {
+        const newSelected = selected+1
+        setSelected(newSelected)
+    }
+
+    return (
+        <div className="carousel-outer-container">
+            {children.map((node, i) => {
+                if (i === selected) return node
+                else return null
+            })}
+            <div className="carousel-button-container">
+                <button className="carousel-button" disabled={selected===0} onClick={handleClickPrevious}>◀</button>
+                <button className="carousel-button" disabled={selected===props.children.length-1} onClick={handleClickNext}>▶</button>
+            </div>
+        </div>
+    )
+}
