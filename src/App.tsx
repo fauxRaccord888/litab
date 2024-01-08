93cfe730472ef9574253cb84240341b3b835207e
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-01-08 21:48:02 +0900

    feat: Register 컴포넌트 추가(가입 사전 질문 기능)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.scss'
+import Register from '$lib/components/Register/Preference'
 import Layout from '$lib/layout/Layout'
 
 function App() {
 
   return (
     <Layout>
+      <Register />
     </Layout>
   )
 }
---
+++ src/lib/constants/components/Register.ts
@@ -0,0 +1,8 @@
+export const message = {
+    kr: {
+        preference: '좋아하는 쪽을 골라주세요'
+    },
+    en: {
+        preference: 'Pick one you favor'
+    }
+}
