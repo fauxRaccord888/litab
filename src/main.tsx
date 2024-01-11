8b7d792d8d3bc83e328d83acd68e2b19d3d37024
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-01-12 00:16:26 +0900

    chore: 모듈 추가(react-hot-toast) 및 관련 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
-
 import { Provider } from 'react-redux'
+import { Toaster } from "react-hot-toast"
+
 import { store } from "./lib/stores/store.ts"
 import { router } from '$lib/router'
 
@@ -11,6 +12,7 @@ import './App.scss'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <React.StrictMode>
+      <Toaster />
       <RouterProvider router={router}/>
     </React.StrictMode>
   </Provider>
