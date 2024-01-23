cd18900bc92bf5444f4fc1f89bb6bb4d31f3917f
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-01-24 08:43:53 +0900

    feat: profile/mutableId Route 에러 및 대기 컴포넌트 추가

--- src/routes/profile/$mutableId.tsx
+++ src/routes/profile/$mutableId.tsx
@@ -1,56 +1,54 @@
 /* route */
 import { FileRoute } from "@tanstack/react-router";
 /* types */
+import type { PropsWithStatus } from "$lib/types/components";
 import type { IHeaderProfileProps } from "$lib/types/components/Profile/Header";
 /* fetch */
-import { getProfileByMutableId_DB } from '$lib/database/profiles';
+import { getProfileByMutableId_DB } from "$lib/database/profiles";
 /* components */
 import ProfileHeader from "$lib/components/Profile/Header";
-/* env */
-const DATABASE_URL = import.meta.env.VITE_PUBLIC_DB_URL;
-const PROFILE_PIC_BUCKET = import.meta.env.VITE_PUBLIC_PROFILE_PIC_BUCKET;
 
 export const Route = new FileRoute('/profile/$mutableId').createRoute({
     parseParams: (params) => ({
         mutableId: params.mutableId,
     }),
-    loader: ({ params: { mutableId } }) => getProfileByMutableId_DB({ mutableId }),
+    loader: async ({ params }) => {
+        const { data } = await getProfileByMutableId_DB({ mutableId: params.mutableId })
+        if (data?.[0]) return data[0]
+        else throw new Error('profile not found')
+    },
+    errorComponent: ({error}) => <ProfileComponent status="error" error={error}/>,
+    pendingComponent: () => <ProfileComponent status="pending" />,
     component: Profile
 })
 
-// TODO profile fetch data를 Props로 변환
-const dummpProfileProps: IHeaderProfileProps = {
-    profile : {
-        mutable_id: 'foo',
-        nickname: 'bar',
-        description: 'baz',
-        id: 'foo',
-        approved: false,
-        preference: [1, 1, 1],
-
-        img_src: `${DATABASE_URL}/${PROFILE_PIC_BUCKET}/foo/profile.jpg`,
-
-        post: 1,
-        follower: 2,
-        following: 3,
-        mutualFollowing: ['foo', 'bar', 'baz'],
-    },
-    settings: {
-        lang: 'kr'
-    },
-    // TODO action 추가 hooks으로
-    action: {
-        handleFollow: () => {},
-        handleShowMore: () => {},
-        handleShowMutualFollowing: () => {}
+function Profile() {
+    const data  = Route.useLoaderData()
+    const profileProps: IHeaderProfileProps = {
+        profile: data,
+        mutualFollower: ['foo', 'bar', 'baz'],
+        settings: {
+            lang: 'kr'
+        },
+        // TODO action 추가 hooks으로
+        action: {
+            handleFollow: () => {},
+            handleShowMore: () => {},
+            handleShowMutualFollowing: () => {},
+            followings: {
+                follower: () => {},
+                following: () => {}
+            }
+        }
     }
+    return <ProfileComponent status="success" {...profileProps}/>
 }
 
-function Profile() {
-    const { data } = Route.useLoaderData()
+// TODO Profile.index에 추가
+function ProfileComponent(props: PropsWithStatus<IHeaderProfileProps>) {
     return (
         <div className="profile-container">
-            <ProfileHeader {...dummpProfileProps} />
+            <ProfileHeader {...props} />
         </div>
     )
 }
