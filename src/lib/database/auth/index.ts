dad1b9c216905bfbafec26d7299b900781a4a522
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-01-28 10:48:58 +0900

    refactor: Supabase Database 관련 함수 및 훅스 삭제

--- src/lib/database/auth/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './registerOnAuth'
-export * from './signIn'
--- src/lib/hooks/mutation/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './profile'
-export * from './register'
-export * from './signIn'
-export * from './signOut'
