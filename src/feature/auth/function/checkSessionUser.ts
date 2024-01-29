502326296425c4a353e47eb1026420659e5d23db
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-01-29 18:43:41 +0900

    feat: auth - Tanstack Router 로드 중 auth 확인 로직 추가

--- src/feature/auth/function/checkSessionUser.ts
+++ src/feature/auth/function/checkSessionUser.ts
@@ -1,4 +1,5 @@
 import type { AppStore } from "$lib/stores/store"
+
 import { supabaseClient } from "$lib/graphql/supabaseClient"
 import { setSessionUser } from "$lib/stores/authSlice"
 
@@ -6,9 +7,10 @@ export async function checkSessionUser(payload: {
     store: AppStore
 }) {
     const { store } = payload
+    const { auth } = store.getState()
     const { data } = await supabaseClient.auth.getSession()
     const sessionUser = data?.session?.user
-    if (sessionUser) {
+    if (sessionUser && sessionUser !== auth.sessionUser) {
         store.dispatch(setSessionUser(sessionUser))
     }
     return sessionUser
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,6 +4,8 @@ import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import type { AppStore } from '$lib/stores/store'
 import type { ApolloClient, NormalizedCacheObject } from '@apollo/client'
 
+import { checkSessionUser, checkTableUser } from '$feature/auth/function'
+
 import Layout from '$lib/layout/Layout'
 import LeftSideBar from '$lib/components/NavigationBar/LeftSideBar'
 import AuthLayout from '$lib/layout/AuthLayout'
@@ -12,7 +14,12 @@ export const Route = rootRouteWithContext<{
   store: AppStore
   apolloClient: ApolloClient<NormalizedCacheObject>
 }>()({
-  component: RootComponent,
+    beforeLoad: async ({ context }) => {
+        const { store, apolloClient } = context
+        checkSessionUser({ store })
+        checkTableUser({ store, client: apolloClient})
+    },
+    component: RootComponent,
 })
 
 function RootComponent() {
