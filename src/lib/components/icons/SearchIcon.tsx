57de8a2eb58153b2d5113603b8634639031803ff
fauxRaccord888 <153690179+fauxRaccord888@users.noreply.github.com>
2024-02-03 00:01:34 +0900

    feat: 검색 컴포넌트 구현 (#12)

---
+++ src/lib/components/icons/SearchIcon.tsx
@@ -0,0 +1,8 @@
+export default function SearchIcon(props?: {className?: string}) {
+    return (
+        <svg className={props?.className} viewBox="0 0 24 24">
+            <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+    )
+}
+  
