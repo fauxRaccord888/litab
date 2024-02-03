31cc66c895505c2440d2d2d32bc41bae52695a6a
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-02-04 00:35:31 +0900

    feat: 유틸 함수(색상 헥스코드를 정수 배열로 변환) 추가 및 버튼 컴포넌트 스타일 props 추가

--- src/lib/utils/rgba.ts
+++ src/lib/utils/rgba.ts
@@ -6,3 +6,12 @@ export const rgba = (rgbArray?: number[] | null) => {
 const toHex = (num: number) => {
     return num.toString(16).padStart(2, '0')
 }
+
+export const fromHex = (hex: string) => {
+    const processedHex = hex.replace('#', '')
+    return [
+        parseInt(processedHex.slice(0,2), 16),
+        parseInt(processedHex.slice(2,4), 16),
+        parseInt(processedHex.slice(4,6), 16)
+    ]
+}
