9dcfd44b4b3f552135f2ebc6e61a59fb8566ebd6
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-02-08 20:15:24 +0900

    refactor: Oeuvre - OeuvreNode 컴포넌트 HoverCard 컴포넌트 적용(부모 요소 z-index 문제)

--- src/feature/Oeuvre/components/OeuvreNode.tsx
+++ src/feature/Oeuvre/components/OeuvreNode.tsx
@@ -1,46 +1,52 @@
 import type { DBOeuvre } from "../types";
+import { useRef } from "react";
 import { useHover } from '$lib/hooks/useHover';
 import BucketImage from "$lib/components/common/BucketImage";
 import FallbackIcon from "$lib/components/icons/FallbackIcon";
 import OeuvreMainInfo from "./common/OeuvreMainInfo";
+import HoverCard from "$feature/portal/components/HoverCard";
 
 import "./style/oeuvreNode.scss"
 
-type OeuvreNodeProps = {
-    oeuvre: DBOeuvre | null | undefined;
+interface OeuvreInfoProps {
+    oeuvre?: DBOeuvre | undefined
 }
 
-export default function OeuvreNode(props: OeuvreNodeProps) {
+export default function OeuvreNode(props: OeuvreInfoProps) {
     const { oeuvre } = props
-    const { status, handleMouseLeave, handleMouseOver} = useHover()
+    const { status, handleMouseLeave, handleMouseOver } = useHover()
+    const ref = useRef<HTMLDivElement | null>(null)
+    const position = ref.current?.getBoundingClientRect()
 
     return (
-        <div 
+        <div
+            ref={ref}
             onMouseOver={handleMouseOver}
             onMouseLeave={handleMouseLeave}
             className="oeuvre-node-container"
         >
-            {oeuvre && 
-                <div
-                    onMouseOver={handleMouseOver}
-                    onMouseLeave={handleMouseLeave} 
-                    className={[
-                        'oeuvre-hover-card-container',
-                        status ? 'show' : ''
-                    ].join(' ')}
+            {oeuvre &&
+                <HoverCard
+                    position={position}
+                    status={status}
+                    handleMouseOver={handleMouseOver}
+                    handleMouseLeave={handleMouseLeave} 
                 >
-                    <OeuvreMainInfo mini {...oeuvre}/>
-                </div>
+                    <div className="oeuvre-hover-card-container">
+                        <OeuvreMainInfo mini {...oeuvre}/>
+                    </div>
+                </HoverCard>
             }
-            {oeuvre && 
-                <div className="oeuvre-cover-container">
+
+            <div className="oeuvre-cover-container">
+                {oeuvre &&
                     <BucketImage 
                         bucket="oeuvres"
                         id={oeuvre.id}
-                        fallback={<FallbackIcon />}
+                        fallback={<FallbackIcon className="fallback-icon"/>}
                     />
-                </div>
-            }
+                }
+            </div>
         </div>
     )
 }
