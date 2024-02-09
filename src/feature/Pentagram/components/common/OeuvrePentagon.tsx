d035b75dc7ec4196c8d8cb9d6afa371680a328df
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-02-09 17:20:26 +0900

    feat: Pentagram - 펜타그램 오각형 컴포넌트 내 서브노드 렌더링 로직 추가

--- src/feature/Pentagram/components/common/OeuvrePentagon.tsx
+++ src/feature/Pentagram/components/common/OeuvrePentagon.tsx
@@ -4,10 +4,11 @@ import type { CSSProperties, MouseEventHandler } from "react";
 
 import OeuvreNode from "$feature/Oeuvre/components/OeuvreNode";
 import PentagonIcon from "$lib/components/icons/PentagonIcon";
+import ItemIterator from "$lib/components/common/ItemIterator";
 
-type Node = FirstNodeOfCollection<DBOeuvresCollection>
-export default function OeuvrePentagon(props: {collection: DBOeuvresCollection | undefined | null}) {
-    const { collection } = props;
+import { getAngleAndDisctance } from "../../utils";
+import { PENTAGRAM } from './../../constants';
+import "./style/oeuvrePentagon.scss"
 
 interface OeuvrePentagonProps {
     mainNodes: DBOeuvresCollection
@@ -22,16 +23,65 @@ export default function OeuvrePentagon(props: OeuvrePentagonProps) {
         items[edge.node.index] = edge.node.oeuvres
     })
 
+    const subItems = subNodes?.edges.map((edge) => {
+        return {
+            ...edge.node,
+            id: edge.node.oeuvres.id // COMMENT 하나의 pentagram에 속한 oeuvre는 유일함이 보장됨
+        }
+    })
+
+    const handleClick: MouseEventHandler<HTMLDivElement> = (e) => {
+        const rect = e.currentTarget.getBoundingClientRect()
+        const { angle, distance, error } = getAngleAndDisctance(e, rect, PENTAGRAM.SIDES)
+        // TODO - PENTAGRAMS_NODES MUTATION
+        console.log(angle, distance, error)
+    }
+
     return (
         <div className="oeuvre-pentagon-container">
-            <div className="parent">
-                {items.map((item) => (
-                    <div key={item?.oeuvres.id} className="main-node">
-                        <OeuvreNode oeuvre={item?.oeuvres} />
-                    </div>
-                ))}
+            <div 
+                onClick={handleClick}
+                className="parent"
+            >
+                <ItemIterator
+                    items={items}
+                    componentFunction={MainNode}
+                />
+                {subItems &&
+                    <ItemIterator
+                        items={subItems}
+                        componentFunction={SubNode}
+                    />
+                }
                 <PentagonIcon className="pentagon"/>
             </div>
+        </div>
+    )
+}
+
+
+interface SubNodeStyle extends CSSProperties {
+    '--offset-multiplier': number,
+    '--degrees': string
+}
+
+function MainNode(props: DBOeuvre | undefined) {
+    return (
+        <div className="node main-node">
+            <OeuvreNode oeuvre={props}/>
+        </div>
+    )
+}
+
+function SubNode(props: DBPentagramsNodes) {
+    const { angle, distance, oeuvres } = props
+    const style: SubNodeStyle = {
+        '--offset-multiplier': distance / 100,
+        '--degrees': `${angle}deg`
+    }
+    return (
+        <div className="node sub-node" style={style}>
+            <OeuvreNode oeuvre={oeuvres} />
         </div>
     )
 }
