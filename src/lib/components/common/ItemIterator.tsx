565cc011a7583bfdf6c6f4b85239b0a94c62d959
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-02-10 19:30:03 +0900

    refactor: ItemIterator - 공용 이터레이터 프롭 추가 기능 추가

--- src/lib/components/common/ItemIterator.tsx
+++ src/lib/components/common/ItemIterator.tsx
@@ -1,21 +1,25 @@
 import { Fragment } from "react"
 
 type MinimalProp = {
-    id: string
-} | null | undefined
+    item: {
+        id?: string 
+    } | null | undefined
+}
 
 interface ItemIteratorProps<T extends MinimalProp> {
     items: T[],
+    additionalProps?: Record<string, unknown>,
     componentFunction: (p: T) => JSX.Element
 }
 
 export default function ItemIterator<T extends MinimalProp>(props: ItemIteratorProps<T>) {
-    const { items, componentFunction } = props
+    const { items, additionalProps, componentFunction } = props
+
     return (
         <>
-            {items.map((item, idx) => (
-                <Fragment key={item?.id || idx}>
-                    {componentFunction(item)}
+            {items.map((i, idx) => (
+                <Fragment key={i.item?.id || idx}>
+                    {componentFunction({...i, ...additionalProps})}
                 </Fragment>
             ))}
         </>
