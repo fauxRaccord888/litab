7342e1e9990d4acc816de22ccac1d5d587187b08
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-02-13 13:14:58 +0900

    chore: Pentagram - 상수 추가(index를 고유값으로 사용하기 위한 Offset)

--- src/feature/Pentagram/constants.ts
+++ src/feature/Pentagram/constants.ts
@@ -3,5 +3,8 @@ export const PENTAGRAM = {
     ANGLE_CALIBRATE: 270, // Y축 방향으로 돌리기 위해서 270도를 돌려야 함
     FULL_CIRCLE: 360,
     HALF_CIRCLE: 180,
-    SIDES: 5
+    SIDES: 5,
+
+    MAIN_NODE_OFFSET: 10000,
+    PENDING_NODE_OFFSET: 20000,
 }
