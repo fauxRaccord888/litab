e536eecc4f59eac450e8c06126f05d58083667b4
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-02-15 23:57:16 +0900

    chore: Pentagram 관련 상수 수정 및 스타일 수정

--- src/feature/Pentagram/constants.ts
+++ src/feature/Pentagram/constants.ts
@@ -1,10 +1,13 @@
 export const PENTAGRAM = {
     EPSILON: 1e-10,
-    ANGLE_CALIBRATE: 270, // Y축 방향으로 돌리기 위해서 270도를 돌려야 함
+    ANGLE_OFFSET: 270, // Y축 방향으로 돌리기 위해서 270도를 돌려야 함
     FULL_CIRCLE: 360,
     HALF_CIRCLE: 180,
     SIDES: 5,
 
-    MAIN_NODE_OFFSET: 10000,
+    VERTEX_ANGLE_THRESHOLD: 5,
+    VERTEX_DISTANCE_THRESHOLD: 10,
+    VERTEX_TARGET_DISTANCE: 100,
+
     PENDING_NODE_OFFSET: 20000,
 }
