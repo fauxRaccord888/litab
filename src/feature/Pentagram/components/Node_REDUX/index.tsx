dd47cc304616664a0c838850107fe1bdfd372fe7
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-02-20 17:25:57 +0900

    refactor: Pentagram - UpdateView 컴포넌트 및 Node 컴포넌트 변경(PendingNode 삭제, quadtree ref hooks, 렌더링 시 merge)

--- src/feature/Pentagram/components/Node_REDUX/index.tsx
+++ src/feature/Pentagram/components/Node_REDUX/index.tsx
@@ -1,38 +1,34 @@
 import type { AppRootState } from '$lib/stores/store';
 import { useSelector } from 'react-redux';
-import { pendingNodeSelector, nodeSelector } from '$feature/Pentagram/store/pentagramUpsertSlice';
+import { useMergedNode, useSelected } from '../../hooks';
 
 import PentagramNode from '../common/PentagramNode';
 
 type HandleDrag = (e: { clientX: number, clientY: number }) => void
 
 type NodeWithPositionParentProps = {
-    id: string
+    id: string,
     handleDrag?: HandleDrag
     handleClickNode?: (id: string) => void
 }
 
 export function Node(props: NodeWithPositionParentProps) {
     const { id, ...restProps } = props
-    const item = useSelector((state: AppRootState) => nodeSelector.selectById(state, id))
+    const mergedNode = useMergedNode(id)
+    const selected = useSelected(id)
 
     return (
-        <PentagramNode
-            {...restProps}
-            item={item}
-        />
-    )
-}
-
-export function PendingNode(props: NodeWithPositionParentProps) {
-    const { id, ...restProps } = props
-    const item = useSelector((state: AppRootState) => pendingNodeSelector.selectById(state, id))
-
-    return (
-        <PentagramNode
-            {...restProps}
-            item={item}
-        />
+        <>
+            {mergedNode &&
+                <PentagramNode
+                    {...restProps}
+                    item={{
+                        ...mergedNode,
+                        selected
+                    }}
+                />
+            }
+        </>
     )
 }
 
--- src/feature/Pentagram/components/PentagramUpdateView.tsx
+++ src/feature/Pentagram/components/PentagramUpdateView.tsx
@@ -1,8 +1,9 @@
 import type { FormatProps } from '$lib/types/components';
 import type { DBPentagram } from "../types";
 import { useRef } from 'react';
-import { useHandleClickNodes, useHandleDrag, usePentagramNodes, useQuadtree } from "../hooks";
-import { Node, PendingNode, SelectedPosition } from './Node_REDUX';
+import { useInitialize, useQuadtreeRef } from '../hooks/store';
+import { useHandleClickNodes, useHandleDrag } from "../hooks";
+import { Node, SelectedPosition } from './Node_REDUX';
 
 import OeuvrePentagonWrapper from "./common/OeuvrePentagonWrapper";
 import ItemIterator from "$lib/components/common/ItemIterator";
@@ -13,37 +14,29 @@ import './style/pentagramUpdateView.scss'
 export default function PentagramUpdateView(props: FormatProps<DBPentagram>) {
     const { item } = props
     const { pentagrams_nodesCollection: nodes } = item
-
-    useQuadtree()
     const parentRef = useRef<HTMLDivElement | null>(null)
-    const { handleDrag } = useHandleDrag(parentRef.current)
-    const { handleClickNode, handleClickPendingNode, handleClickParent } = useHandleClickNodes()
-    const { nodeIds, pendingNodeIds } = usePentagramNodes(nodes)
-
+    const ids = useInitialize(nodes)
+    const quadtreeRef = useQuadtreeRef()
+    const { handleDrag } = useHandleDrag(parentRef.current, quadtreeRef.current)
+    const { handleClickNode, handleClickParent } = useHandleClickNodes()
+    
     return (
         <div className="pentagram-update-view-container">
             <OeuvrePentagonWrapper 
                 ref={parentRef}
                 handleClickParent={handleClickParent}    
             >
-                {nodeIds &&
+                {ids &&
                     <ItemIterator
                         additionalProps={{
                             handleDrag,
-                            handleClickNode
+                            handleClickNode,
                         }}
-                        items={nodeIds}
+                        items={ids.map((id) => ({
+                            id,
+                            quadtreeRef: quadtreeRef.current
+                        }))}
                         componentFunction={Node}
-                    />
-                }
-                {pendingNodeIds &&
-                    <ItemIterator
-                        additionalProps={{
-                            handleDrag,
-                            handleClickNode: handleClickPendingNode
-                        }}
-                        items={pendingNodeIds}
-                        componentFunction={PendingNode}
                     />
                 }
                 <SelectedPosition handleDrag={handleDrag} />
