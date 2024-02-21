e517afe5556bc459a19d2914fe7b4490f7d87990
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-02-21 20:05:44 +0900

    fix: Pentagram - 클릭 이벤트 핸들러 내 충돌 확인 조건 누락 수정

--- src/feature/Pentagram/hooks/eventHandler/useHandleClickNodes.ts
+++ src/feature/Pentagram/hooks/eventHandler/useHandleClickNodes.ts
@@ -1,10 +1,12 @@
 import type { MouseEventHandler } from "react"
+import type { QuadtreeNode } from "../../utils"
 import { useDispatch } from "react-redux"
 import { setSelected, setSelectedPosition } from "../../store/pentagramUpsertSlice"
 import { getAngleAndDisctance } from "../../utils"
+import { Quadtree } from "../../utils"
 import { PENTAGRAM } from "../../constants"
 
-export function useHandleClickNodes() {
+export function useHandleClickNodes(quadtreeRef: QuadtreeNode | null) {
     const dispatch = useDispatch()
 
     const handleClickNode = (id: string) => {
@@ -12,10 +14,15 @@ export function useHandleClickNodes() {
     }
 
     const handleClickParent: MouseEventHandler<HTMLDivElement> = (e) => {
+        if (!quadtreeRef) return
+
         const { angle, distance } = getAngleAndDisctance(e, e.currentTarget, PENTAGRAM.SIDES)
-        if (typeof angle === 'number' && typeof distance === 'number') {
-            dispatch(setSelectedPosition({angle, distance}))
-        }
+        if (typeof angle !== 'number' || typeof distance !== 'number') return
+
+        const colliding = Quadtree.checkCollidingByPosition(quadtreeRef, { angle, distance })
+        if (colliding) return
+        
+        dispatch(setSelectedPosition({angle, distance}))
     }
 
     return { handleClickNode, handleClickParent }
