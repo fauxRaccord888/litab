5956389273d82acfc161d709cb4762323b19f369
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-02-25 23:04:54 +0900

    reafctor: Pentagram Update 스토어 리팩토링
    1. 인터페이스 수정
    2. 노드 merge 시 upsert가 얕은 업데이트를 하는 부분 수정(set으로 변경)
    3. 변경 사항 삭제 및 추가를 위한 메서드 추가
    4. 변경 사항을 upsert로 통일하기 위해 작품 검색 후 upsert하는 부분을 update로 통일
    5. 드래그 시 발생 액션(노드의 위치 설정, 신규 위치 설정)을 슬라이스에서 통합 - 불필요 Selector 호출 감소

--- src/feature/Pentagram/store/pentagramUpsertSlice/interface.ts
+++ src/feature/Pentagram/store/pentagramUpsertSlice/interface.ts
@@ -1,14 +1,25 @@
 import type { DBOeuvre } from "$feature/Oeuvre/types"
 
-export interface Node {
-    id: string,
+export interface INode {
+    id: string
     oeuvres: DBOeuvre,
     angle: number,
     distance: number,
 }
 
-export type PendingChange = (UpsertChange | UpdateChange | RemoveChange) 
+export interface IMergedNode extends INode {
+    deleted?: boolean,
+    selected?: boolean
+}
+
+export type IPendingChange = (UpsertChange | UpdateChange | RemoveChange) 
 
 export type UpsertChange = { id: string, changeType: 'upsert', angle: number, distance: number, oeuvres: DBOeuvre }
 export type UpdateChange = { id: string, changeType: 'update', angle: number, distance: number }
 export type RemoveChange = { id: string, changeType: 'remove' }
+
+export type IUnmergedChangeInfo = {
+    id: string
+    node: INode,
+    change: IPendingChange
+}
