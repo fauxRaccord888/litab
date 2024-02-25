527c27006cce485ff60db0ce4feb0aed89baac01
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-02-25 23:39:11 +0900

    feat: Deep compare 유틸 함수 추가 - 노드 병합 시 불필요 렌더링 축소 목적

---
+++ src/lib/utils/deepCompare.ts
@@ -0,0 +1,47 @@
+export function deepCompare(obj1: unknown, obj2: unknown) {
+    if (typeof obj1 !== typeof obj2) {
+        return false;
+    }
+
+    if (obj1 === null && obj2 === null) {
+        return true;
+    }
+
+    if (typeof obj1 !== 'object' || typeof obj2 !== 'object') {
+        return obj1 === obj2;
+    }
+
+    if (Array.isArray(obj1) && Array.isArray(obj2)) {
+        if (obj1.length !== obj2.length) {
+            return false;
+        }
+        for (let i = 0; i < obj1.length; i++) {
+            if (!deepCompare(obj1[i], obj2[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    if (obj1 === null || obj2 === null) return false
+
+    const keys1 = Object.keys(obj1);
+    const keys2 = Object.keys(obj2);
+    if (keys1.length !== keys2.length) {
+        return false;
+    }
+
+    for (const [key, value] of Object.entries(obj1)) {
+        try {
+            hasKey(obj2, key)
+            return deepCompare(value, obj2[key])
+        } catch(e) {
+            return false
+        }
+    }
+    return true;
+}
+
+function hasKey<T extends object>(obj: T, key: string | number | symbol): asserts key is (keyof T) {
+    if (!(key in obj)) throw new Error()
+}
--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -2,3 +2,4 @@ export * from './formatProps'
 export * from './getRelativeTimeAndUnit'
 export * from './luminance'
 export * from './rgba'
+export * from "./deepCompare"
