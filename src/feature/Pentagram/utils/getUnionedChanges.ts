5212152b1556244e8ff76a8d50c38c62c540e424
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-03-06 15:35:01 +0900

    feat: 펜타그램 유틸 함수(일부 속성 제외, DB 엔티티 가공) 추가

---
+++ src/feature/Pentagram/utils/getUnionedChanges.ts
@@ -0,0 +1,49 @@
+import type { RecoverRecordInfoFragment, RemoveRecordInfoFragment, UpdateRecordInfoFragment, UpsertRecordInfoFragment } from "$lib/graphql/__generated__/graphql";
+import type { ChangeTypeKey, PentagramChangeProps } from "../components/common/PentagramChange";
+import { formatProps } from "$lib/utils";
+
+type EntityWithUnunionedChanges= {
+    pentagram_revision_upsert_recordsCollection?: UpsertRecordInfoFragment | null | undefined,
+    pentagram_revision_update_recordsCollection?: UpdateRecordInfoFragment | null | undefined,
+    pentagram_revision_remove_recordsCollection?: RemoveRecordInfoFragment | null | undefined,
+    pentagram_revision_recover_recordsCollection?: RecoverRecordInfoFragment | null | undefined
+} | null | undefined
+
+export function getUnionedChanges(node: EntityWithUnunionedChanges):PentagramChangeProps[] {
+    if (!node) return []
+
+    const { 
+        pentagram_revision_upsert_recordsCollection: upsert,
+        pentagram_revision_update_recordsCollection: update,
+        pentagram_revision_remove_recordsCollection: remove,
+        pentagram_revision_recover_recordsCollection: recover
+    } = node
+
+    const unprocessedChanges = {
+        upsert,
+        update,
+        remove,
+        recover
+    }
+
+    const keys:ChangeTypeKey[] = ["upsert", "update", "remove", "recover"]
+    const result: PentagramChangeProps[] = keys
+        .map((key) => (
+            unprocessedChanges[key]?.edges.map((edge) => formatProps({
+                ...edge.node,
+                created_at: edge.node.pentagram_revisions.created_at,
+                oeuvres: edge.node.pentagram_nodes.oeuvres,
+                changeType: key
+            }))
+        ))
+        .reduce((acc: PentagramChangeProps[], cur) => {
+            if (!cur) return acc
+            return acc?.concat(cur)
+        }, [])
+        .sort((a, b) => {
+            if (!a.item?.created_at || !b.item?.created_at) return 0
+            return new Date(b.item?.created_at).getTime() - new Date(a.item?.created_at).getTime()
+        })
+
+        return result
+}
