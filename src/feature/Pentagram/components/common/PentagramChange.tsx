cbe95a0d418977d867a47c3770deea7d8d94566e
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-03-06 15:22:40 +0900

    refactor: 펜타그램 Changes 컴포넌트 분리

---
+++ src/feature/Pentagram/components/common/PentagramChange.tsx
@@ -0,0 +1,65 @@
+import type { FormatProps } from "$lib/types/components";
+import type { DBOeuvre } from "$feature/Oeuvre/types";
+import { Trans } from "react-i18next";
+import PositionIcon from "$lib/components/icons/PositionIcon";
+import "./style/pentagramChanges.scss"
+
+export type ChangeTypeKey = "upsert" | "update" | "remove" | "recover"
+export type PentagramChangeProps = FormatProps<{
+    changeType: ChangeTypeKey,
+    oeuvres: DBOeuvre,
+    previous_angle?: number | undefined | null
+    previous_distance?: number | undefined | null
+    current_angle?: number | undefined | null
+    current_distance?: number | undefined | null
+
+    created_at?: string
+}>
+
+export default function PentagramChange(props: PentagramChangeProps) {
+    const { changeType, oeuvres, previous_angle, previous_distance, current_angle, current_distance } = props.item
+
+    return (
+        <div className="pentagram-change-component">
+            <div className="pentagram-change-component__main-info-container">
+                <span className="pentagram-change-component__change-type">
+                    <Trans i18nKey={`pentagram.changes.${changeType}`}/>
+                </span>
+                <span className="pentagram-change-component__title">{oeuvres.title}</span>
+            </div>
+            
+            <div className="pentagram-change-component__position-info-container">
+                {
+                    (
+                        typeof previous_angle === 'number' &&
+                        typeof previous_distance === 'number'
+                    ) &&
+                    <div className="pentagram-change-component__position-container">
+                        <PositionIcon 
+                            className={[
+                                "pentagram-change-component__position-icon-component",
+                                "pentagram-change-component__position-icon-component--previous"
+                            ].join(" ")} 
+                        />
+                        {`${previous_angle}° ${previous_distance}%`}
+                    </div>
+                }
+                {
+                    (
+                        typeof current_angle === 'number' &&
+                        typeof current_distance === 'number'
+                    ) &&
+                    <div className="pentagram-change-component__position-container">
+                        <PositionIcon 
+                            className={[
+                                "pentagram-change-component__position-icon-component",
+                                "pentagram-change-component__position-icon-component--current"
+                            ].join(" ")} 
+                        />
+                        {`${current_angle}° ${current_distance}%`}
+                    </div>
+                }
+            </div>
+        </div>
+    )    
+}
