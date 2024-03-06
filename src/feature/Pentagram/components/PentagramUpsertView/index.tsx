d5c021f3b685cec948925bb4e07d5d3abb23b604
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-03-06 15:29:00 +0900

    feat: 펜타그램 UpsertView 컴포넌트 작성 및 임시 저장 확인용 Dialog 추가

---
+++ src/feature/Pentagram/components/PentagramUpsertView/index.tsx
@@ -0,0 +1,39 @@
+import type { ForwardedRef, MouseEvent } from "react";
+import type { PentagramUpsertEditorProps } from "./PentagramUpsertEditor";
+import { useTranslation } from "react-i18next";
+import { forwardRef } from "react";
+
+import PentagramUpsertEditor from "./PentagramUpsertEditor";
+import Button from "$lib/components/common/Button";
+
+import "./style/pentagramUpsertView.scss"
+
+type PentagramUpsertViewProps = PentagramUpsertEditorProps & {
+    title: string,
+    handleSubmit: () => void;
+}
+
+export default forwardRef(function PentagramUpsertView(props: PentagramUpsertViewProps, ref: ForwardedRef<HTMLDivElement>) {
+    const { title, handleSubmit, ...editorProps } = props 
+    const { t } = useTranslation()
+
+    const onClickSubmit = (e: MouseEvent) => {
+        e.stopPropagation()
+        if (handleSubmit) handleSubmit()
+    }
+
+    return (
+        <div className="pentagram-upsert-view-component">
+            <span className="pentagram-upsert-view-component__title">
+                {title}
+            </span>
+            <PentagramUpsertEditor 
+                ref={ref}
+                {...editorProps} 
+            />
+            <Button className="pentagram-upsert-view-component__submit-button" primary onClick={onClickSubmit}>
+                {t('button.submit')}
+            </Button>
+        </div>
+    )
+})
