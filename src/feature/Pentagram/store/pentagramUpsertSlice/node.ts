f39201f23935eb8b5324774bf36f8381e481a0d8
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-03-11 16:32:57 +0900

    refactor: 펜타그램 업데이트 시, 위치 초기화 방식 변경(snapshot 계산)

--- src/feature/Pentagram/store/pentagramUpsertSlice/node.ts
+++ src/feature/Pentagram/store/pentagramUpsertSlice/node.ts
@@ -2,6 +2,7 @@ import type { DBPentagram_UPDATE } from "$feature/Pentagram/types"
 import type { PayloadAction } from "@reduxjs/toolkit"
 import type { UpdateNodeState } from "."
 import { nodeAdapter } from "."
+import { getSnapshot, getUnionedChanges } from "../../utils"
 
 export const initializeNode = (state: UpdateNodeState, action: PayloadAction<{
     nodes: DBPentagram_UPDATE["pentagram_nodesCollection"] | undefined
@@ -9,7 +10,10 @@ export const initializeNode = (state: UpdateNodeState, action: PayloadAction<{
     const { nodes } = action.payload
     nodeAdapter.setAll(state.node, [])
     nodes && nodes.edges.forEach((edge) => {
-        const { id, angle, distance, deleted, oeuvres } = edge.node
+        const { id, oeuvres } = edge.node
+        const unionedChanges = getUnionedChanges(edge.node)
+        const { angle, distance, deleted } = getSnapshot(unionedChanges, new Date())
+        if (typeof angle !== 'number' || typeof distance !== 'number') return
         nodeAdapter.upsertOne(state.node, {
             id,
             angle,
