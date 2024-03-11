714cc0390e4498f80a132f84b653e5c32adb590b
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-03-11 16:51:15 +0900

    fix: 펜타그램 수정 페이지 내 임시작성 본문 무조건적 초기화 오류 수정

--- src/feature/Pentagram/hooks/dataProcess/useInitialize.ts
+++ src/feature/Pentagram/hooks/dataProcess/useInitialize.ts
@@ -1,7 +1,7 @@
 import type { DBPentagram_UPDATE } from "../../types"
 import { useEffect } from "react"
 import { useDispatch } from "react-redux"
-import { initialize, setDescription } from "../../store/pentagramUpsertSlice"
+import { initialize } from "../../store/pentagramUpsertSlice"
 
 export function useInitialize(pentagram: DBPentagram_UPDATE | null) {
     const dispatch = useDispatch()
@@ -10,10 +10,5 @@ export function useInitialize(pentagram: DBPentagram_UPDATE | null) {
         dispatch(
             initialize( { nodes: pentagram?.pentagram_nodesCollection } )
         )
-        if (pentagram?.description) {
-            dispatch(
-                setDescription(pentagram?.description)
-            )
-        }
     }, [pentagram, dispatch])
 }
