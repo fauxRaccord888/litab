5e2a3d71dffbaf72b81b6dfc2d88601a2de714eb
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-03-13 19:52:09 +0900

    chore: auth 기능 hooks 불필요 타이핑 제거

--- src/feature/auth/hooks/useSignOutMutation.ts
+++ src/feature/auth/hooks/useSignOutMutation.ts
@@ -1,30 +1,22 @@
-import { useDispatch } from "react-redux"
-import { useCallback, useState } from "react"
-
 import type { AuthError } from "@supabase/supabase-js"
-
+import { useCallback, useState } from "react"
 import { signOut_SUPABASE } from "../supabase"
-import { resetUserInfo } from "$lib/stores/authSlice"
 
-export type AuthSignOutMutation = () => Promise<{error: AuthError | null}>
-type Status = { error: null | AuthError, loading: boolean}
-
-export function useSignOutMutation(): [AuthSignOutMutation, Status] {
+export function useSignOutMutation(): [typeof mutation, typeof status] {
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState<AuthError | null>(null)
-    const dispatch = useDispatch()
+    const status = { loading, error }
     
     const mutation = useCallback(() => {
         setLoading(true)
         const response = signOut_SUPABASE()
-        dispatch(resetUserInfo())
         response.then((response) => {
             const { error: supabaseError } = response
             setLoading(false)
             if (supabaseError) setError(supabaseError)
         })
         return response
-    }, [dispatch])
+    }, [])
 
-    return [mutation, { loading, error }]
+    return [mutation, status]
 }
