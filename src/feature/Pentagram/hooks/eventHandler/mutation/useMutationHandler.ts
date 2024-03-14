d9831e0621335d1466b2f0b42414ad1082bd9eb8
fauxRaccord888 <153690179+fauxRaccord888@users.noreply.github.com>
2024-03-14 16:45:32 +0900

    fix: 펜타그램 생성 및 업데이트 시, 중복 생성 문제 해결(pending 로직 추가) (#49)

--- src/feature/Pentagram/hooks/eventHandler/mutation/useMutationHandler.ts
+++ src/feature/Pentagram/hooks/eventHandler/mutation/useMutationHandler.ts
@@ -1,14 +1,15 @@
 import type { AppRootState } from "$lib/stores/store";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useUnmergedChangeInfo } from "../..";
 import { supabaseClient } from "$lib/supabase/client";
 import { abortChanges } from "../../../store/pentagramUpsertSlice";
-import { InsertPentagramTransactionError, UpdatePentagramTransactionError } from "../../../error";
+import { InsertPentagramTransactionError, PendingError, UpdatePentagramTransactionError } from "../../../error";
 import { filterChanges, formatChanges } from '../../../utils';
 
 export function useMutationHandler() {
     const dispatch = useDispatch()
+    const [pending, setPending] = useState(false)
     const { description } = useSelector((state: AppRootState) => state.pentagramUpsert)
     const changes = useUnmergedChangeInfo()
     const filteredChanges = filterChanges(changes)
@@ -20,36 +21,46 @@ export function useMutationHandler() {
     } = formatChanges(filteredChanges)
 
     const handleInsertPentagram = useCallback(async () => {
+        if (pending) throw new PendingError()
+        setPending(true)
         const { data, error } =await supabaseClient
             .rpc('batched_insert_pentagram', {
                 pentagram: {
-                    description: description
+                    description: description || ""
                 },
                 upsert_changes: processedUpsertChanges,
             })
-
+        if (data || error) {
+            setPending(false)
+        }
+    
         if (error) throw new UpdatePentagramTransactionError()
         dispatch(abortChanges())
         return data
-    }, [description, dispatch, processedUpsertChanges])
+    }, [description, dispatch, processedUpsertChanges, pending])
 
     const handleUpdatePentagram = useCallback(async (pentagramId: string) => {
+        if (pending) throw new PendingError()
+        setPending(true)
         const { data, error } =await supabaseClient
             .rpc('batched_update_pentagram', {
                 pentagram_id: pentagramId,
                 pentagram: {
-                    description: description
+                    description: description || ""
                 },
                 upsert_changes: processedUpsertChanges,
                 update_changes: processedUpdateChanges,
                 remove_changes: processedRemoveChanges,
                 recover_changes: processedRecoverChanges,
             })
+        if (data || error) {
+            setPending(false)
+        }
 
         if (error) throw new InsertPentagramTransactionError()
         dispatch(abortChanges())
         return data
-        }, [description, dispatch, processedRecoverChanges, processedRemoveChanges, processedUpdateChanges, processedUpsertChanges])
+    }, [description, dispatch, pending, processedRecoverChanges, processedRemoveChanges, processedUpdateChanges, processedUpsertChanges])
 
 
     return { handleInsertPentagram, handleUpdatePentagram }
