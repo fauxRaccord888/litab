951bc63894219a9c70aeccb8a592cdcc6e3a5cc6
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-03-14 16:14:31 +0900

    chore: 공용 types 추가

--- src/lib/types/components/index.ts
+++ src/lib/types/components/index.ts
@@ -1,8 +1,20 @@
+import type { DBAuthUser } from "$feature/auth/type"
+import type { CSSProperties } from "react"
+
 export type FormatProps<T=unknown> = {
     id?: string | number
     item: T
 }
 
-export type Nullable<T extends FormatProps> = {
-    item: T["item"] | undefined | null
+export type Nullable<T> = Partial<T> & {
+    id: string
+}
+
+export interface CustomBackgroundCSSProp extends CSSProperties {
+    "--custom-bg": number[],
+}
+
+
+export type RouterContext = {
+    currentUser?: DBAuthUser | null | undefined,
 }
