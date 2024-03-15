9d5376cc0526879bab1ce492921e1c66c2d23119
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-03-15 19:20:27 +0900

    chore: InfoCard 컴포넌트 타입 이름 변경

--- src/feature/template/type.ts
+++ src/feature/template/type.ts
@@ -1,3 +1,4 @@
 export type ISlotItem = React.JSX.Element | string | undefined | null
 export type ISlotComponent<K extends string> = Record<K, ISlotItem>
-export type ISlotRenderConfig<K extends string, V=boolean> = Record<K, V>
+export type ISlotRenderConfig<K extends string> = Record<K, boolean>
+export type ISlotOptions<K extends string, V extends (boolean | string)> = Record<K, V>
