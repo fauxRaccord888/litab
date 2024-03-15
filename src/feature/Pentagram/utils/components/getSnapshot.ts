96366a4e95ef231e7545a93f40e126cc9adac58f
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-03-15 19:17:16 +0900

    chore: ItemIterator 삭제로 인한 데이터 가공 util 함수 변경

--- src/feature/Pentagram/utils/components/getSnapshot.ts
+++ src/feature/Pentagram/utils/components/getSnapshot.ts
@@ -6,25 +6,25 @@ export function getSnapshot(
     timestamp: Date
 ) {
     const position = unionedChanges.find((change) => {
-        if (!change?.item) return false
-        if (!change.item?.created_at) return false
-        if (new Date(change.item?.created_at) > timestamp) return false
-        if (typeof change.item?.current_angle !== 'number') return false
-        if (typeof change.item?.current_distance !== 'number') return false
+        if (!change) return false
+        if (!change?.created_at) return false
+        if (new Date(change?.created_at) > timestamp) return false
+        if (typeof change?.current_angle !== 'number') return false
+        if (typeof change?.current_distance !== 'number') return false
         return true
     })
 
     const deleted = unionedChanges
         .find((change) => {
-            if (!change?.item) return false
-            if (!change.item?.created_at) return false
-            if (new Date(change.item?.created_at) > timestamp) return false
+            if (!change) return false
+            if (!change?.created_at) return false
+            if (new Date(change?.created_at) > timestamp) return false
             return true
         })
-        ?.item.changeType === 'remove'
+        ?.changeType === 'remove'
 
-    const angle = position ? position.item.current_angle : null;
-    const distance = position ? position.item.current_distance : null;
+    const angle = position ? position.current_angle : null;
+    const distance = position ? position.current_distance : null;
 
     return { angle, distance, deleted }
 }
--- src/feature/Pentagram/utils/components/getUnionedChanges.ts
+++ src/feature/Pentagram/utils/components/getUnionedChanges.ts
@@ -1,7 +1,6 @@
 import type { RecoverRecordInfoFragment, RemoveRecordInfoFragment, UpdateRecordInfoFragment, UpsertRecordInfoFragment } from "$lib/graphql/__generated__/graphql";
-import type { ChangeTypeKey, PentagramChangeProps } from "../../components/common/PentagramChange";
+import type { ChangeTypeKey, TPentagramChange } from "../../components/common/PentagramChange";
 import type { Collections } from "$lib/types/graphql";
-import { formatProps } from "$lib/utils";
 
 type EntityWithUnunionedChanges= {
     pentagram_revision_upsert_recordsCollection?: Collections<UpsertRecordInfoFragment> | null | undefined,
@@ -10,7 +9,7 @@ type EntityWithUnunionedChanges= {
     pentagram_revision_recover_recordsCollection?: Collections<RecoverRecordInfoFragment> | null | undefined
 } | null | undefined
 
-export function getUnionedChanges(node: EntityWithUnunionedChanges):PentagramChangeProps[] {
+export function getUnionedChanges(node: EntityWithUnunionedChanges):TPentagramChange[] {
     if (!node) return []
 
     const { 
@@ -28,22 +27,22 @@ export function getUnionedChanges(node: EntityWithUnunionedChanges):PentagramCha
     }
 
     const keys:ChangeTypeKey[] = ["upsert", "update", "remove", "recover"]
-    const result: PentagramChangeProps[] = keys
+    const result: TPentagramChange[] = keys
         .map((key) => (
-            unprocessedChanges[key]?.edges?.map((edge) => formatProps({
+            unprocessedChanges[key]?.edges?.map((edge) => ({
                 ...edge.node,
                 created_at: edge.node.pentagram_revisions.created_at,
                 oeuvres: edge.node.pentagram_nodes.oeuvres,
                 changeType: key
             }))
         ))
-        .reduce((acc: PentagramChangeProps[], cur) => {
+        .reduce((acc: TPentagramChange[], cur) => {
             if (!cur) return acc
             return acc?.concat(cur)
         }, [])
         .sort((a, b) => {
-            if (!a.item?.created_at || !b.item?.created_at) return 0
-            return new Date(b.item?.created_at).getTime() - new Date(a.item?.created_at).getTime()
+            if (!a?.created_at || !b?.created_at) return 0
+            return new Date(b?.created_at).getTime() - new Date(a?.created_at).getTime()
         })
 
     return result
