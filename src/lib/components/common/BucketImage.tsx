d5940ae6bf1bcbff8c4f40a46b2a9d02548e4e3c
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-03-17 13:22:58 +0900

    chore: BucketImage 컴포넌트 버킷 추가 및 드래그 고스트 문제 수정

--- src/lib/components/common/BucketImage.tsx
+++ src/lib/components/common/BucketImage.tsx
@@ -1,23 +1,29 @@
+import type { MouseEvent } from "react";
 import { useState } from "react"
 import { getURLfromBucket } from "$lib/supabase/storage"
 import "./style/bucketImage.scss"
 
-export type BucketKeys = 'profiles' | 'oeuvres'
+export type BucketKeys = 'profiles' | 'oeuvres' | "artists" | "genres"
 export interface BucketImageProps {
     id: string
     bucket: BucketKeys
     fallback?: JSX.Element
+    className?: string,
+    onClick?: (e: MouseEvent) => void
 }
 
 export default function BucketImage(props: BucketImageProps) {
-    const { id, fallback, bucket, ...restProps } = props
+    const { id, fallback, bucket, className, onClick, ...restProps } = props
     const [error, setError] = useState(false)
     const url = getURLfromBucket({id, bucket, file: FILE[bucket]})
 
     return (
-        <div className="bucket-image-component">
+        <div 
+            onClick={onClick} 
+            className={`${className || ""} bucket-image-component`}
+        >
             {!error && 
-                <img className="image" {...restProps} src={url} loading="lazy" onError={() => setError(true)}/>
+                <img draggable={false} className="image" {...restProps} src={url} loading="lazy" onError={() => setError(true)}/>
             }
             {error && fallback}
         </div>
@@ -26,5 +32,7 @@ export default function BucketImage(props: BucketImageProps) {
 
 const FILE: Record<BucketKeys, string> = {
     profiles: 'profile.jpg',
-    oeuvres: 'cover.jpg'
+    oeuvres: 'cover.jpg',
+    artists: "profile.jpg",
+    genres: "cover.jpg"
 }
