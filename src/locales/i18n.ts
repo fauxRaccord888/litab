d17dac9340274a4a0b1adfe400a284091c145f48
fauxRaccord888 <153690179+fauxRaccord888@users.noreply.github.com>
2024-03-21 18:51:49 +0900

    chore: i18n 번역 JSON 파일 분리(엔티티/기능별 분리) (#68)

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -1,29 +1,29 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import translationEN from "./en/translation.json";
-import translationKO from "./ko/translation.json";
+import translationEN from "./en";
+import translationKO from "./ko";
 
 const resources = {
-  en: {
-    translations: translationEN
-  },
-  ko: {
-    translations: translationKO
-  }
+    en: {
+        translations: translationEN
+    },
+    ko: {
+        translations: translationKO
+    }
 };
 
 i18n
-  .use(initReactI18next)
-  .init({
-    resources,
-    lng: "ko",
-    fallbackLng: "en",
-    
-    ns: ["translations"],
-    defaultNS: "translations",
-    interpolation: {
-      escapeValue: false
-    }
-  });
+    .use(initReactI18next)
+    .init({
+        resources,
+        lng: "ko",
+        fallbackLng: "en",
+
+        ns: ["translations"],
+        defaultNS: "translations",
+        interpolation: {
+            escapeValue: false
+        }
+    });
 
 export default i18n;
