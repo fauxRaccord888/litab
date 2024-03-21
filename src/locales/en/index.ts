937616946b526ba193ae6177242c7a6766fce803
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-03-21 18:50:59 +0900

    chore: i18n 번역 JSON 파일 분리(엔티티/기능별 분리)

---
+++ src/locales/en/index.ts
@@ -0,0 +1,29 @@
+import account from './account.json'
+import artist from './artist.json'
+import button from './button.json'
+import feed from './feed.json'
+import form from './form.json'
+import genre from './genre.json'
+import modal from './modal.json'
+import navigate from './navigate.json'
+import network from './network.json'
+import node from './node.json'
+import oeuvre from './oeuvre.json'
+import pentagram from './pentagram.json'
+import profile from './profile.json'
+
+export default {
+    account,
+    artist,
+    button,
+    feed,
+    form,
+    genre,
+    modal,
+    navigate,
+    network,
+    node,
+    oeuvre,
+    pentagram,
+    profile
+}
