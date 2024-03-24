7bf788691a1afc6de4fbc5eadffb331fa77fb635
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-03-24 21:30:35 +0900

    feat: 피드 컴포넌트 추가

--- src/feature/feed/components/FeedItem.tsx
+++ src/feature/feed/components/FeedItem.tsx
@@ -1,24 +1,56 @@
-import type { FeedItems } from '../type';
-import type { FormatProps } from '$lib/types/components';
+import type { ISlotRenderConfig } from '$feature/template/type';
+import type { PentagramEventHandler } from '$feature/Pentagram/types';
+import type { OeuvreEventHandler } from '$feature/Oeuvre/types';
+import type { UnprocessedContext } from '$lib/types/components';
+import type { FeedItemOptions, FeedItemRenderConfig, FeedItems } from '../types';
 
 import FeedMessage from "./common/FeedMessage";
-import PentagramSelectView from '$feature/Pentagram/components/PentagramSelectView';
+import PentagramCard from '$feature/Pentagram/components/PentagramSelectView';
+
 import "./style/feedItem.scss"
 
-export default function FeedItem(props: FormatProps<FeedItems>) {
-    let feedItem = null
-    const { item } = props
+export type FeedItmeConfigProps = {
+    renderConfig: ISlotRenderConfig<FeedItemRenderConfig>,
+    options: FeedItemOptions,
+    eventHandler: PentagramEventHandler & OeuvreEventHandler
+    context: UnprocessedContext
+}
 
-    if (item.__typename === 'pentagrams') {
-        feedItem = <PentagramSelectView feed item={item} />
+type FeedItemProps = FeedItmeConfigProps & {
+    item: FeedItems
+}
+
+export default function FeedItem(props: FeedItemProps) {
+    const { item, eventHandler, options, context } = props
+    let feedItem = null
+
+    if (item.__typename === 'pentagram_revisions') {
+        const { pentagrams, id } = item
+    
+        feedItem = (
+            <PentagramCard 
+                item={pentagrams}
+                renderConfig={{
+                    metaInfo: true,
+                    mainPentagon: true,
+                    description: true,
+                    revision: true
+                }}
+                options={{
+                    horizontalMain: true,
+                    displayRevisionIds: [id],
+                }}
+                timestamp={new Date(item.created_at)}
+                eventHandler={eventHandler}
+                context={context}
+            />
+        )
     }
 
     return (
-        <div className="feed-item-container">
-            <div className="inner-container">
-                <FeedMessage {...props}/>
-                {feedItem}
-            </div>
+        <div className="feed-item-component">
+            {options.showMessage && <FeedMessage item={item}/>}
+            {feedItem}
         </div>
     )
 }
--- src/feature/feed/components/common/FeedMessage.tsx
+++ src/feature/feed/components/common/FeedMessage.tsx
@@ -1,29 +1,40 @@
-import type { FeedItems } from "../..//type"
-import type { FormatProps } from '$lib/types/components';
-
+import type { FeedItems } from "../../types"
 import { useTranslation } from "react-i18next"
 import PentagonIcon from "$lib/components/icons/PentagonIcon"
+import PenIcon from "$lib/components/icons/PenIcon"
 
 import "./style/feedMessage.scss"
 
-export default function FeedMessage(props: FormatProps<FeedItems>) {
+type FeedMessageProps = {
+    item: FeedItems
+}
+
+export default function FeedMessage(props: FeedMessageProps) {
     const { t } = useTranslation()
     const { item } = props
-    const { __typename, users } = item
+    const { __typename, id, users } = item
     let icon = null
     let message = null
         
-    if (__typename === "pentagrams") {
-        message = t('feed.message.pentagram', { user: users.mutable_id })
-        icon = <PentagonIcon className="pentagram-icon"/>
+    if (__typename === "pentagram_revisions") {
+        const revisionEdge = item?.pentagrams.pentagram_revisionsCollection?.edges.at(-1)
+        const revisionId = revisionEdge?.node.id
+        const isFirstRevision = revisionId === id
+        if (isFirstRevision) {
+            message = t('feed.message.create', { user: users.mutable_id })
+            icon = <PenIcon className="feed-message-component__pen-icon"/>
+        } else {
+            message = t('feed.message.edit', { user: users.mutable_id })
+            icon = <PentagonIcon className="feed-message-component__pentagram-icon"/>
+        }
     }
 
     return (
         <>
             {icon && message &&
-                <div className="feed-message-container">
-                    <span className="icon-container">{icon}</span>
-                    <span className="meta-info">{message}</span>
+                <div className="feed-message-component">
+                    <span className="feed-message-component__icon-container">{icon}</span>
+                    <span className="feed-message-component__message">{message}</span>
                 </div>
             }
         </>
