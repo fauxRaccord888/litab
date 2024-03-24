75a5549f0a65121f37a3813ab5a08876207087b7
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-03-24 21:02:10 +0900

    feat: 계정 이벤트 핸들러 추가

---
+++ src/feature/Account/hooks/EventHandler/useUpdateProfileHandler.ts
@@ -0,0 +1,88 @@
+import type { UpdateProfileMutation } from "$lib/graphql/__generated__/graphql";
+import type { UpdateAuthPayload, UpdateProfilePayload, UploadCoverPayload } from "../../types";
+import { useCallback } from "react";
+import { useCurrentUser } from "$feature/auth/hooks/useCurrentUser";
+import { useApolloClient, useMutation } from "@apollo/client";
+import { useUpdateAuthMutation, useUploadCoverMutation } from "..";
+import { updateProfile_MUTATION } from "../../graphql/updateProfile";
+import { evictCacheById } from "$lib/utils";
+import { PendingError } from "$lib/error";
+import { getUserById_QUERY } from "$feature/auth/graphql";
+
+export function useUpdateProfileHandler() {
+    const apolloClient = useApolloClient()
+    const currentUser = useCurrentUser()
+    const [profileMutation, profileResponse] = useMutation<UpdateProfileMutation>(updateProfile_MUTATION)
+    const [authMutation, authResponse] = useUpdateAuthMutation() 
+    const [uploadCoverMutation, coverResponse] = useUploadCoverMutation()
+    const isPending = (
+        profileResponse.loading ||
+        authResponse.loading ||
+        coverResponse.loading
+    )
+
+    const profileUpdateHandler = useCallback(async (
+        payload: UpdateProfilePayload,
+    ) => {
+        if (isPending) throw new PendingError()
+        // COMMENT apollo가 던지면 바깥 스코프에서 핸들링
+        const response = await profileMutation({
+            variables: payload,
+            update(cache) {
+                evictCacheById({
+                    cache, 
+                    entity: 'users', 
+                    id: currentUser?.id
+                })
+            },
+            refetchQueries: [
+                getUserById_QUERY
+            ],
+        })
+        return response
+    }, [isPending, profileMutation, currentUser])
+
+    const authUpdateHandler = useCallback(async (
+        payload: UpdateAuthPayload,
+    ) => {
+        if (isPending) throw new PendingError()
+
+        const response = await authMutation(payload)
+        if (response.error) {
+            throw response.error
+        }
+        return response
+    }, [authMutation, isPending])
+
+    const uploadCoverHandler = useCallback(async (
+        payload: UploadCoverPayload,
+    ) => {
+        if (isPending) throw new PendingError()
+
+        const response = await uploadCoverMutation(payload)
+        if (response.error) {
+            throw response.error
+        }
+        if (response.data) {
+            apolloClient.refetchQueries({
+                updateCache(cache) {
+                    evictCacheById({
+                        cache, 
+                        entity: 'users', 
+                        id: currentUser?.id
+                    })
+                },
+                include: [
+                    getUserById_QUERY
+                ]
+            })
+        }
+        return response
+    }, [apolloClient, currentUser, isPending, uploadCoverMutation])
+
+    return { 
+        profile: [profileUpdateHandler, profileResponse] as [typeof profileUpdateHandler, typeof profileResponse], 
+        auth: [authUpdateHandler, authResponse] as [typeof authUpdateHandler, typeof authResponse],
+        cover: [uploadCoverHandler, coverResponse] as [typeof uploadCoverHandler, typeof coverResponse]
+    }
+}
