c4c20f023ba6ca4f69327087dde985674509f5f4
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-03-24 21:04:07 +0900

    feat: 계정 Form 관련 필드 상수 수정

--- src/feature/Account/constants.ts
+++ src/feature/Account/constants.ts
@@ -1,4 +1,4 @@
-import type { InputProps } from "$lib/components/common/Input"
+import type { InputMinimalProps } from "$lib/components/common/Input"
 import LockIcon from "$lib/components/icons/LockIcon"
 import MailIcon from "$lib/components/icons/MailIcon"
 import PenIcon from "$lib/components/icons/PenIcon"
@@ -6,60 +6,60 @@ import TagIcon from "$lib/components/icons/TagIcon"
 import UserIcon from "$lib/components/icons/UserIcon"
 
 export type SignInKey = "email" | 'password'
-export const signInFields: InputProps<SignInKey>[] = [
+export const signInFields: InputMinimalProps<SignInKey>[] = [
     {
         label: 'email', 
-        icon: MailIcon,
+        iconFunction: MailIcon,
     },
     {
         label: 'password', 
-        icon: LockIcon,
+        iconFunction: LockIcon,
         type: 'password',
     },
 ]
 
 export type AccountInsertKeys = 'email' | 'password'
-export const accountInsertFields: (InputProps<AccountInsertKeys> & { regex: string })[] = [
+export const accountInsertFields: (InputMinimalProps<AccountInsertKeys> & { regex: string })[] = [
     {
         label: 'email',
-        icon: MailIcon,
+        iconFunction: MailIcon,
         // eslint-disable-next-line no-useless-escape
         regex: '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,5}$',
     },
     {
         label: 'password',
-        icon: LockIcon,
+        iconFunction: LockIcon,
         type: 'password',
         regex: '^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9!@#$%^&*()._-]{8,16}$',
     },
 ]
 
 export type AccountUpdateKeys = 'newPassword'
-export const accountUpdateFields: (InputProps<AccountUpdateKeys> & { regex: string })[] = [
+export const accountUpdateFields: (InputMinimalProps<AccountUpdateKeys> & { regex: string })[] = [
     {
         label: 'newPassword',
-        icon: LockIcon,
+        iconFunction: LockIcon,
         type: 'password',
         regex: '^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9!@#$%^&*()._-]{8,16}$',
     },
 ]
 
 export type AcocuntProfileUpdateKey = 'mutableId' | 'nickname' | 'description'
-export const accountProfileUpdateFields: (InputProps<AcocuntProfileUpdateKey> & { regex: string })[] = [
+export const accountProfileUpdateFields: (InputMinimalProps<AcocuntProfileUpdateKey> & { regex: string })[] = [
     {
         label: 'mutableId',
-        icon: UserIcon,
+        iconFunction: UserIcon,
         // eslint-disable-next-line no-useless-escape
         regex: '^[a-zA-Z0-9._%+-]{4,15}$',
     },
     {
         label: 'nickname',
-        icon: TagIcon,
+        iconFunction: TagIcon,
         regex: '^(?=.*[a-zA-Z0-9가-힣])[a-zA-Z0-9가-힣]{1,15}$',
     },
     {
         label: 'description',
-        icon: PenIcon,
+        iconFunction: PenIcon,
         regex: '^(?=.*[a-zA-Z0-9가-힣 ])[a-zA-Z0-9가-힣 ]{0,31}$',
     },
 ]
