507932bc2b4fac3fc721852b4482b2115948c6cc
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-03-24 18:36:31 +0900

    refactor: 검색 패널 컴포넌트 드롭다운 추가 등
    1. 드롭다운 컴포넌트 추가
    2. 이벤트에 문자열(입력값) 대신 폼 이벤트 전달
    3. Input 라벨 상수 별도 선언(form 이벤트의 name으로 사용 예정)

--- src/lib/components/common/SearchPanel.tsx
+++ src/lib/components/common/SearchPanel.tsx
@@ -1,30 +1,38 @@
 import type { FormEventHandler } from "react"
+import type { DropdownProps } from "./Dropdown"
 import { useState } from "react"
 import { useTranslation } from "react-i18next"
 import Button from "./Button"
+import Dropdown from "./Dropdown"
 import Input from "./Input"
 import SearchIcon from "../icons/SearchIcon"
+import { SEARCH } from "$feature/search/constants"
 
 import "./style/searchPanel.scss"
 
 interface SearchPanelProps {
-    submitFunction: (keyword: string) => void
+    dropdownProps?: DropdownProps
+    submitFunction: (e: FormData) => void
 }
 
 export default function SearchPanel(props: SearchPanelProps) {
-    const { submitFunction } = props
+    const { submitFunction, dropdownProps } = props
     const [keyword, setKeyword] = useState('')
     const { t } = useTranslation()
 
     const onSubmit:FormEventHandler<HTMLFormElement> = (e) => {
         e.preventDefault()
-        submitFunction(keyword)    
+        const formData = new FormData(e.currentTarget)
+        submitFunction(formData)
     }
 
     return (
         <div className="search-panel-component">
             <form onSubmit={onSubmit} className="search-panel-component__search-form">
-                <Input label="search" icon={SearchIcon} value={keyword} onChange={setKeyword}/>
+                <div className="search-panel-component__search_bar">
+                    {dropdownProps && <Dropdown {...dropdownProps} />}
+                    <Input label={SEARCH.inputName} iconFunction={SearchIcon} value={keyword} onChange={setKeyword}/>
+                </div>
                 <Button primary>{t(`button.search`)}</Button>
             </form>
         </div>
