ddae9b5112deb138f942bd26cb4e25d5bbe52288
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-03-25 00:10:05 +0900

    chore: 불필요 파일 삭제, 불필요 타입 삭제, 불필요 라우트 scrollKey 삭제

--- src/feature/navigate/utils/getProcessedContext.ts
+++
@@ -1,8 +0,0 @@
-import type { ProcessedContext, UnprocessedContext } from "$lib/types/components"
-import { getCurrentUserFromContext } from "../../auth/utils"
-
-export function getProcessedContext(context: UnprocessedContext): ProcessedContext {
-    const currentUser = getCurrentUserFromContext(context)
-
-    return { currentUser, ...context }
-}
