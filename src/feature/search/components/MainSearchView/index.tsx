1e87957df446e61eec48db7d622f6dbf74db615d
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-03-31 20:45:38 +0900

    feat: 검색 컴포넌트 pagination 기능 추가

--- src/feature/search/components/MainSearchView/index.tsx
+++ src/feature/search/components/MainSearchView/index.tsx
@@ -1,5 +1,6 @@
-import type { MainSearchViewEventHandler, SearchDropdownProps, SearchResults } from "../../types"
+import type { MainSearchViewEventHandler, QueryResults, SearchDropdownKey, SearchDropdownProps } from "../../types"
 import type { UnprocessedContext } from "$lib/types/components"
+import { useState } from "react"
 import { isSearchDropDownKey } from "../../types"
 import { SEARCH } from "$feature/search/constants"
 import SearchPanel from "$lib/components/common/SearchPanel"
@@ -7,30 +8,48 @@ import SearchResult from "./SearchResult"
 import "./style/mainSearchView.scss"
 
 type MainSearchViewProps = {
-    searchResult: SearchResults | null
+    queryResults: QueryResults
     dropdownProps: SearchDropdownProps
     context: UnprocessedContext
     eventHandler: MainSearchViewEventHandler
 }
 
 export default function MainSearchView(props: MainSearchViewProps) {
-    const { searchResult, dropdownProps, context, eventHandler } = props
+    const { queryResults, dropdownProps, context, eventHandler } = props
+    const [keyword, setKeyword] = useState('')
+    const [category, setCategory] = useState<SearchDropdownKey | null>(null)
 
     const handleSearch = (formData: FormData) => {
-        const keyword = formData.get(SEARCH.inputName)?.toString()
-        const category = formData.get(SEARCH.dropdownName)?.toString()
+        setCategory(null)
+        setKeyword('')
+        const formKeyword = formData.get(SEARCH.inputName)?.toString()
+        const formCategory = formData.get(SEARCH.dropdownName)?.toString()
 
-        if (!keyword) return
+        if (!formKeyword) return
+        if (!formCategory) return
+        if (!isSearchDropDownKey(formCategory)) return
+        setCategory(formCategory)
+        setKeyword(formKeyword)
+        eventHandler.search(formCategory, formKeyword)
+    }
+
+    const handleLoadMore = () => {
         if (!category) return
         if (!isSearchDropDownKey(category)) return
-        eventHandler.search(category, keyword)
+        eventHandler.search(category, keyword, true)
     }
 
     return (
         <div className="main-search-view-component">
             <div className="main-search-view-component__inner-container">
                 <SearchPanel dropdownProps={dropdownProps} submitFunction={handleSearch} />
-                <SearchResult results={searchResult} context={context} eventHandler={eventHandler} />
+                <SearchResult 
+                    category={category} 
+                    queryResults={queryResults} 
+                    context={context} 
+                    loadMoreFunction={handleLoadMore} 
+                    eventHandler={eventHandler} 
+                />
             </div>
         </div>
     )
