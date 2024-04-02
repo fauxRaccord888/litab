59d46ad365bd3bd9e5cdc9cfa7420d9c5c8d8639
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-04-02 18:47:40 +0900

    feat: 펜타그램 플레이어 컴포넌트 추가

---
+++ src/feature/Pentagram/components/PentagramCard/SelectPlayer/index.tsx
@@ -0,0 +1,55 @@
+import type { DBPentagram_SELECT, PentagramEventHandler } from "../../../types";
+import { useMemo } from "react";
+import PlayIcon from '$lib/components/icons/PlayIcon';
+import "./style/selectPlayer.scss"
+
+type SelectPlayerProps = {
+    timestamp: Date
+    pentagram_revisionsCollection: DBPentagram_SELECT["pentagram_revisionsCollection"],
+    eventHandler: PentagramEventHandler
+}
+
+export default function SelectPlayer(props: SelectPlayerProps) {    
+    const { timestamp, pentagram_revisionsCollection, eventHandler } = props
+
+    const timestamps = useMemo(() => (
+        pentagram_revisionsCollection?.edges
+            .map((edge) => new Date(edge.node.created_at))
+            .sort((a, b) => a.getTime() - b.getTime()) 
+    ), [pentagram_revisionsCollection])
+
+    const onClickPlay = () => {
+        if (eventHandler.playPentagram) {
+            eventHandler.playPentagram()
+        }
+    }
+
+    const onClickProgressBar = (date: Date) => {
+        if (eventHandler.setTimestamp) {
+            eventHandler.setTimestamp(date)
+        }
+    }
+
+    return (
+        <div className="select-player-component">
+            <div className="select-player-component__inner-container">
+                <div className="select-player-component__play-button" onClick={onClickPlay}>
+                    <PlayIcon />
+                </div>
+                <div className="select-player-component__progress-bar">
+                    {timestamps?.map((t) => (
+                        <span 
+                            title={String(new Date(t))} 
+                            className={[
+                                "select-player-component__progress-node",
+                                t.getTime() === timestamp.getTime() ? "select-player-component__progress-node--current" : ""
+                            ].join(" ")} 
+                            onClick={() => onClickProgressBar(t)}
+                            
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
