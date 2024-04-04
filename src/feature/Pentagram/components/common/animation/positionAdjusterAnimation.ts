2dffbc40505703af131f2db5175fc9c1e5889fb9
fauxRaccord888 <153690179+fauxRaccord888@users.noreply.github.com>
2024-04-04 10:55:08 +0900

    refactor: 위치 조정 컴포넌트 애니메이션 및 속성 변경 (#99)

--- src/feature/Pentagram/components/common/animation/positionAdjusterAnimation.ts
+++ src/feature/Pentagram/components/common/animation/positionAdjusterAnimation.ts
@@ -1,28 +1,42 @@
 import type { PentagramNodePosition } from "../../../types"
+import type { useCSSVariables } from "$lib/hooks/useCSSVariables"
 import { calcTransformValueFromPosition } from "../../../utils"
-import { useCSSVariables } from "$lib/hooks/useCSSVariables"
+
+// COMMENT
+// from 부분은 신규 생성 노드의 애니메이션을 위해 필요
 
 export function calcPositionAdjusterAnimation(payload: {
     position: PentagramNodePosition,
     prevPosition?: PentagramNodePosition | undefined,
     enableAnimation?: boolean,
+    shadowDeleted?: boolean | null | undefined
     STYLE: ReturnType<typeof useCSSVariables>
 }) {
-    const { position, prevPosition, enableAnimation, STYLE } = payload
+    const { position, prevPosition, enableAnimation, shadowDeleted, STYLE } = payload
     const { transformX, transformY } = calcTransformValueFromPosition(position, STYLE)
     const { transformX: prevX, transformY: prevY } = calcTransformValueFromPosition(prevPosition, STYLE)
     const isValidCurPosition = typeof transformX === 'number' && typeof transformY === 'number'
     const isValidPrevPosition = typeof prevX === 'number' && typeof prevY === 'number'
 
+    const fromScale = (!isValidPrevPosition || prevPosition?.deleted) ? 0 : 1
+    const fromTranslate = (enableAnimation && isValidPrevPosition)
+        ? `${prevX}px, ${prevY}px`
+        : `${transformX}px, ${transformY}px` 
+
+    const toScale = shadowDeleted ? 1
+        : (!isValidCurPosition || position.deleted) ?  0 
+        : 1
+    const toTranslate = `${transformX}px, ${transformY}px`
+
     return {
         from: { 
-            opacity: (!isValidPrevPosition || prevPosition?.deleted) ?  0 : 1,
-            transform: (enableAnimation && isValidPrevPosition) 
-                ? `translate(${prevX}px, ${prevY}px)` 
-                : `translate(${transformX}px, ${transformY}px)` 
+            transform: `translate(${fromTranslate}) scale(${fromScale})`
         },
-        opacity: (!isValidCurPosition || position.deleted) ?  0 : 1,
-        transform: `translate(${transformX}px, ${transformY}px)` 
+        transform: `translate(${toTranslate}) scale(${toScale})`,
+        config: { 
+            mass: 3,
+            friction: 40,
+            tension: 280,
+        }
     }
-    
 }
