51060af307627a04ab0e5ead7f553433f081dbb0
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-04-05 21:44:24 +0900

    feat: 펜타그램 작품 오각형 컴포넌트에 inView state 추가 및 props 전달

--- src/feature/Pentagram/components/PentagramCard/SelectMainPentagon/index.tsx
+++ src/feature/Pentagram/components/PentagramCard/SelectMainPentagon/index.tsx
@@ -1,4 +1,6 @@
 import type { DBPentagram_SELECT, PentagramEventHandler, PentagramSelectOptions } from "../../../types";
+import { useIntersectionObserver } from "$lib/hooks";
+import { useRef, useState } from "react";
 import OeuvrePentagonWrapper from "../../common/OeuvrePentagonWrapper";
 import PentagramNode from "./PentagramNode";
 
@@ -14,6 +16,11 @@ type SelectMainPentagonProps = {
 export default function SelectMainPentagon(props: SelectMainPentagonProps) {
     const { timestamp, pentagram_nodesCollection, options, eventHandler } = props
     const items = pentagram_nodesCollection?.edges.map((edge) => edge.node)
+    const sentinelRef = useRef<HTMLDivElement | null>(null)
+    const [inView, setInView] = useState(false)
+    
+    const onIntersect = () => setInView(true)
+    useIntersectionObserver(sentinelRef, onIntersect)
 
     return pentagram_nodesCollection && (
         <div className="select-main-pentagon-component">
@@ -21,6 +28,7 @@ export default function SelectMainPentagon(props: SelectMainPentagonProps) {
                 {items?.map((item) => (
                     <PentagramNode 
                         key={item.id}
+                        inView={inView}
                         item={item}
                         options={options}
                         eventHandler={eventHandler}
@@ -28,6 +36,7 @@ export default function SelectMainPentagon(props: SelectMainPentagonProps) {
                     />
                 ))}
             </OeuvrePentagonWrapper>
+            {!inView && <div ref={sentinelRef} className="select-main-pentagon-component__sentinel" />}
         </div>
     )
 }
