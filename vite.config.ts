7b7bcbb1956f8294f5002ec48c736805849dbfa8
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-04-09 19:55:06 +0900

    chore: mdx 관련 패키지 설치 및 vite 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import path from "path";
 import react from '@vitejs/plugin-react-swc'
 import codegen from 'vite-plugin-graphql-codegen';
+import mdx from '@mdx-js/rollup'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
+        {enforce: 'pre', ...mdx()},
         TanStackRouterVite({
             routeFileIgnorePrefix: '-'
         }),
