e3fe16a555dac3fe07c0afd419afd3d6daf271ea
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-04-10 19:05:56 +0900

    chore: 펜타그램 mutation 에러 핸들러 추가(변경 사항 없을 때 별도 메시지 출력)

---
+++ src/feature/Pentagram/errorHandler/index.ts
@@ -0,0 +1,23 @@
+import { CustomUnknownError } from "$lib/error"
+import { isCustomError, isError } from "$lib/utils"
+import { EmptyInputError, PentagramTransactionError } from "../error/MutationError"
+
+export async function pentagramMutationErrorHandler<T>(func: () => T) {
+    try {
+        const result = await func()
+        return result
+    } catch(e) {
+        if (!isError(e)) {
+            throw new CustomUnknownError()
+        }
+
+        if (e.message === 'empty input') {
+            throw new EmptyInputError()
+        }
+
+        if (isCustomError(e)) {
+            throw e
+        }
+        throw new PentagramTransactionError()
+    }
+}
