2cb37118e65b92f5874c8df582a690bf6e2621e4
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-04-12 15:23:16 +0900

    refactor: 모달 slice 추가(search param 기반에서 store 기반으로)

---
+++ src/feature/portal/store/modalSlice.ts
@@ -0,0 +1,105 @@
+import type { AppRootState } from '$lib/stores/store';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+
+type ModalSliceState = {
+    nodeUpsertId?: string | undefined
+    insertNode?: boolean | undefined
+    pentagramMenuId?: string | undefined
+    nodeViewId?: string | undefined
+    revisionViewId?: string | undefined
+    accountMenu?: boolean | undefined
+    register?: boolean | undefined
+    signIn?: boolean | undefined
+}
+
+const initialState = {
+    nodeUpsertId: undefined,
+    insertNode: undefined,
+    pentagramMenuId: undefined,
+    nodeViewId: undefined,
+    revisionViewId: undefined,
+    accountMenu: undefined,
+    register: undefined,
+    signIn: undefined,
+} satisfies ModalSliceState
+
+const modalSlice = createSlice({
+    name: 'modal',
+    initialState: initialState as ModalSliceState,
+    reducers: {
+        closeModal() {
+            return initialState
+        },
+
+        setNodeUpsertId(_, action: PayloadAction<string>) {
+            return {
+                ...initialState,
+                nodeUpsertId: action.payload
+            }
+        },
+
+        setInsertNode() {
+            return {
+                ...initialState,
+                insertNode: true
+            }
+        },
+
+        setPentagramMenuId(_, action: PayloadAction<string>) {
+            return {
+                ...initialState,
+                pentagramMenuId: action.payload
+            }
+        },
+
+        setNodeViewId(_, action: PayloadAction<string>) {
+            return {
+                ...initialState,
+                nodeViewId: action.payload
+            }
+        },
+        
+        setRevisionViewId(_, action: PayloadAction<string>) {
+            return {
+                ...initialState,
+                revisionViewId: action.payload
+            }
+        },
+
+        setAccountMenu() {
+            return {
+                ...initialState,
+                accountMenu: true
+            }
+        },
+
+        setRegister() {
+            return {
+                ...initialState,
+                register: true
+            }
+        },
+
+        setSignIn() {
+            return {
+                ...initialState,
+                signIn: true
+            }
+        }
+    },
+})
+
+export type AuthState = AppRootState["modal"]
+export const { 
+    closeModal, 
+    setNodeUpsertId, 
+    setInsertNode, 
+    setPentagramMenuId, 
+    setNodeViewId, 
+    setRevisionViewId, 
+    setAccountMenu, 
+    setRegister, 
+    setSignIn 
+} = modalSlice.actions
+export default modalSlice.reducer
