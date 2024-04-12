1d8a97cb3e9da0cc01e9394db7d468f8ab920da5
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-04-12 14:57:42 +0900

    feat: 펜타그램 메뉴 모달에 삭제 기능 추가

--- src/feature/Pentagram/components/Modal/PentagramMenuModal.tsx
+++ src/feature/Pentagram/components/Modal/PentagramMenuModal.tsx
@@ -1,7 +1,9 @@
 import type { MouseEvent } from "react";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import Modal from "$feature/portal/components/Modal";
 import Button from "$lib/components/common/Button";
+import Confirm from "$feature/portal/components/Confirm";
 import "./style/pentagramMenuModal.scss"
 
 type PentagramMenuModalProps = {
@@ -9,46 +11,87 @@ type PentagramMenuModalProps = {
     isAuthor?: boolean,
     handleClickClose: () => void
     handleClickUpdate: () => void,
+    handleClickDelete: () => void,
     handleClickNavigate: () => void
 }
 
 export default function PentagramMenuModal(props: PentagramMenuModalProps) {
-    const { title, isAuthor, handleClickClose, handleClickUpdate, handleClickNavigate } = props
+    const { title, isAuthor, handleClickClose, handleClickUpdate, handleClickDelete, handleClickNavigate } = props
     const { t } = useTranslation()
+    const [deleteConfirm, setDeleteConfirm] = useState(false)
+    
+    const onClickNavigate = (e: MouseEvent) => {
+        e.stopPropagation()
+        handleClickNavigate()
+    }
 
     const onClickUpdate = (e: MouseEvent) => {
         e.stopPropagation()
         handleClickUpdate()
     }
 
-    const onClickNavigate = (e: MouseEvent) => {
+    const onClickDelete = (e: MouseEvent) => {
         e.stopPropagation()
-        handleClickNavigate()
+        setDeleteConfirm(true)
     }
 
+    const handleConfirmDelete = () => {
+        handleClickDelete()
+        setDeleteConfirm(false)
+    }
+
+    const publicMenus = (
+        <>
+            <Button 
+                className={[
+                    "pentagram-menu-modal-component__button"
+                ].join(' ')}
+                onClick={onClickNavigate}
+            >
+                {t("pentagram.menu.navigate")}
+            </Button>
+        </>
+    )
+
+    const authorMenus = (
+        <>
+            <Button 
+                className={[
+                    "pentagram-menu-modal-component__button--update"
+                ].join(' ')}
+                onClick={onClickUpdate}
+            >
+                {t("pentagram.menu.update")}
+            </Button>
+
+            <Button 
+                className={[
+                    "pentagram-menu-modal-component__button--delete"
+                ].join(' ')}
+                onClick={onClickDelete}
+            >
+                {t("pentagram.menu.delete")}
+            </Button>
+        </>
+    )
+
     return (
-        <Modal title={title} handleClickClose={handleClickClose}>
-            <div className="pentagram-menu-modal-component__inner-cotainer">
-                <Button 
-                    className={[
-                        "pentagram-menu-modal-component__button"
-                    ].join(' ')}
-                    onClick={onClickNavigate}
+        <>
+            {deleteConfirm && 
+                <Confirm 
+                    destructive={true}
+                    handleClickAffirmative={handleConfirmDelete}
+                    handleClickNegative={() => setDeleteConfirm(false)}
                 >
-                    {t("pentagram.menu.navigate")}
-                </Button>
-                {
-                    isAuthor &&
-                    <Button 
-                        className={[
-                            "pentagram-menu-modal-component__button--update"
-                        ].join(' ')}
-                        onClick={onClickUpdate}
-                    >
-                        {t("pentagram.menu.update")}
-                    </Button>
-                }
-            </div>
-        </Modal>
+                    {t('portal.confirm.message.deletePentagram')}
+                </Confirm>
+            }
+            <Modal title={title} handleClickClose={handleClickClose}>
+                <div className="pentagram-menu-modal-component__inner-cotainer">
+                    {publicMenus}
+                    {isAuthor && authorMenus}
+                </div>
+            </Modal>
+        </>
     )
 }
