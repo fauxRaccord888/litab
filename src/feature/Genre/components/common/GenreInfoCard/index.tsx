881357903f7f493aec06c780a0169ccaeeb265aa
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-04-15 11:28:31 +0900

    feat: 장르 인포카드 내 업데이트 옵션 및 업데이트 바로가기 추가

--- src/feature/Genre/components/common/GenreInfoCard/index.tsx
+++ src/feature/Genre/components/common/GenreInfoCard/index.tsx
@@ -2,9 +2,13 @@ import type { ISlotRenderConfig } from "$feature/template/type";
 import type { InfoCardOptions, InfoCardRenderConfigKey } from "$feature/template/components/InfoCardTemplate"
 import type { DBGenre, GenreEventHandler } from "../../../types";
 import { useMemo } from 'react';
+import { useTranslation } from "react-i18next";
+import Button from "$lib/components/common/Button";
 import BucketImage from "$lib/components/common/BucketImage";
 import FallbackIcon from "$lib/components/icons/FallbackIcon";
 import InfoCardTemplate from "$feature/template/components/InfoCardTemplate";
+
+import "./style/genreInfoCard.scss"
 
 type GenreInfoCardProps = {
     item: DBGenre
@@ -16,10 +20,29 @@ type GenreInfoCardProps = {
 export default function GenreInfoCard(props: GenreInfoCardProps) {
     const { item, renderConfig, options, eventHandler } = props
     const { id, name, abstract, updated_at, description } = item
+    const { t } = useTranslation()
 
     const handleSelect = () => {
         if (eventHandler.selectGenre) eventHandler.selectGenre(item)
     }
+
+    const handleUpdate = () => {
+        if (eventHandler.updateGenre) eventHandler.updateGenre(item)
+    }
+
+    const titleComponent = (
+        <div className="genre-info-card-component__title">
+            <span>{name}</span>
+            {options?.enableUpdate &&
+                <Button
+                    className="genre-info-card-component__button--update" 
+                    onClick={() => handleUpdate()}
+                >
+                    {t('button.update')}
+                </Button>
+            }
+        </div>
+    )
 
     const coverImage = useMemo(() => (
         <BucketImage bucket="genres" timeStamp={updated_at} id={id} fallback={<FallbackIcon />} />
@@ -33,7 +56,7 @@ export default function GenreInfoCard(props: GenreInfoCardProps) {
                 onClick={handleSelect}
                 renderConfig={renderConfig}
                 components={{
-                    title: name,
+                    title: titleComponent,
                     coverImage,
                     mainInfo: abstract,
                     subInfo: description
