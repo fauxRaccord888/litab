ecd8416e2948e1bdb0e00eafb9d420812d49bb7b
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-04-15 11:28:55 +0900

    feat: 작품 인포카드 내 업데이트 옵션 및 업데이트 바로가기 추가

--- src/feature/Oeuvre/components/common/OeuvreInfoCard/index.tsx
+++ src/feature/Oeuvre/components/common/OeuvreInfoCard/index.tsx
@@ -2,10 +2,13 @@ import type { ISlotRenderConfig } from "$feature/template/type";
 import type { InfoCardOptions, InfoCardRenderConfigKey } from "$feature/template/components/InfoCardTemplate"
 import type { DBOeuvre, OeuvreEventHandler } from "../../../types";
 import { useMemo } from 'react';
+import { useTranslation } from "react-i18next";
 import BucketImage from "$lib/components/common/BucketImage";
 import FallbackIcon from "$lib/components/icons/FallbackIcon";
 import InfoCardTemplate from "$feature/template/components/InfoCardTemplate";
 import OeuvreMainInfo from "./OeuvreMainInfo";
+import Button from "$lib/components/common/Button";
+import "./style/oeuvreInfoCard.scss"
 
 type OeuvreInfoProps = {
     item: DBOeuvre
@@ -17,10 +20,29 @@ type OeuvreInfoProps = {
 export default function OeuvreInfoCard(props: OeuvreInfoProps) {
     const { item, renderConfig, options, eventHandler } = props
     const { id, title, description, updated_at } = item
+    const { t } = useTranslation()
 
     const handleSelect = () => {
         if (eventHandler.selectOeuvre) eventHandler.selectOeuvre(item)
     }
+
+    const handleUpdate = () => {
+        if (eventHandler.updateOeuvre) eventHandler.updateOeuvre(item)
+    }
+
+    const titleComponent = (
+        <div className="oeuvre-info-card-component__title">
+            <span>{title}</span>
+            {options?.enableUpdate &&
+                <Button 
+                    className="oeuvre-info-card-component__button--update" 
+                    onClick={() => handleUpdate()}
+                >
+                    {t('button.update')}
+                </Button>
+            }
+        </div>
+    )
 
     const coverImage = useMemo(() => (
         <BucketImage bucket="oeuvres" timeStamp={updated_at} id={id} fallback={<FallbackIcon />} />
@@ -40,9 +62,9 @@ export default function OeuvreInfoCard(props: OeuvreInfoProps) {
                 onClick={handleSelect}
                 renderConfig={renderConfig}
                 components={{
-                    title,
                     coverImage,
                     mainInfo,
+                    title: titleComponent,
                     subInfo: description
                 }}
                 options={{
