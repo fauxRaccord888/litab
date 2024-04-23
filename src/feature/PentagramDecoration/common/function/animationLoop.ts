c20ed8ec02daaf82a1ca7c6eeea2f9a54e7ef309
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-04-23 23:52:07 +0900

    refactor: 펜타그램 꾸미기 애니메이션 루프 종료 조건 및 해제 함수 옵션 추가

--- src/feature/PentagramDecoration/common/function/animationLoop.ts
+++ src/feature/PentagramDecoration/common/function/animationLoop.ts
@@ -1,18 +1,30 @@
 // TODO 
 // ref오브젝트로 사용하기 때문에 class 구현체를 써도 상관이 없음
-// 트리 셰이킹이 안 되는 부분 및 메모리 누수 관련 부분에서 class 활용 검토 필요
+// 50ms마다 실행하는 코드이기 때문에 적절한 메모리 관리가 필요함
 
-import type { IDynamicObject } from "../types"
+import type { IDynamicObject } from "../../types"
 
-export function animationLoop(
-    dynamics: IDynamicObject[] | null,
+export function animationLoop(args: {
+    dynamics: (IDynamicObject | null)[] | null,
     ctx: CanvasRenderingContext2D,
     canvas: HTMLCanvasElement,
-) {
+    onComplete?: (() => void) | null,
+}) {
+    const { dynamics, ctx, canvas, onComplete } = args
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     if (!dynamics) return
-    dynamics.forEach((d) => {
+    
+    for (let i = 0; i < dynamics.length; i++) {
+        const d = dynamics[i]
+        if (!d) continue
         d.update(canvas, ctx)
         d.draw(canvas, ctx)
-    })
+        if (d.completed) {
+            dynamics[i] = null
+        }
+    }
+
+    if (dynamics.every((d) => d === null)) {
+        if (onComplete) onComplete()
+    }
 }
