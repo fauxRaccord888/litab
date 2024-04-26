75e342af191dcc628d86b52a7b1ce8b7efeb55fb
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-04-26 20:58:50 +0900

    refactor: 도형 내부 랜덤 지점 함수 내 prng 매개변수 추가

--- src/lib/utils/pickPointInside.ts
+++ src/lib/utils/pickPointInside.ts
@@ -5,13 +5,13 @@ export function pickPointInside(width: number, height: number, sides: number): [
     const xArgs = { min: -width/2, max: width/2, skew: 1 }
     const yArgs = { min: -height/2, max: height/2, skew: 1 }
 
-    let x = boxMullerRandom(xArgs)
-    let y = boxMullerRandom(yArgs)
+    let x = boxMullerRandom({...xArgs, prng: Math.random})
+    let y = boxMullerRandom({...yArgs, prng: Math.random})
 
     const radius = width / 2
     while (!isInside(y, x, radius, sides)) {
-        x = boxMullerRandom(xArgs)
-        y = boxMullerRandom(yArgs)
+        x = boxMullerRandom({...xArgs, prng: Math.random})
+        y = boxMullerRandom({...yArgs, prng: Math.random})
     }
     x += width/2
     y += height/2
