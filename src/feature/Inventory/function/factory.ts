869a7012f1ce067c51e0f3f4b83f9aff6512bba4
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-04-26 20:55:17 +0900

    feat: 펜타그램 꾸미기 팩토리 메서드 변경(시드만 생성)

--- src/feature/Inventory/function/factory.ts
+++ src/feature/Inventory/function/factory.ts
@@ -1,33 +1,56 @@
-import type { InventoryEntities, InventoryKey } from "../types";
+import type { DBDecoration } from "../types";
 import type { IDynamicObject } from "$feature/PentagramDecoration";
 import { Cosmos, Ocean, Shadow } from "$feature/PentagramDecoration";
+import { DecorationType } from "$lib/graphql/__generated__/graphql";
 
 type AnimationInstance = Cosmos | Shadow | Ocean
 
 export class SeedFactory {
     // TODO 이후 인벤토리 아이템 추가 시, "직접" 추가해줘야 함
     static cstr = {
-        Ocean,
-        Cosmos, 
-        Shadows: Shadow
+        ocean: Ocean,
+        cosmos: Cosmos, 
+        shadow: Shadow
     }
 
-    static createSeed(category: InventoryKey) {
-        const cstr = SeedFactory.cstr[category]
-        if (!cstr) return null
-        return cstr.createSeed()
+    static createSeed() {
+        return Math.floor(Math.random() * 0xffffffffffff).toString(16).padEnd(12, "0")
     }
 
     static createAnimationInstance(
-        seed: InventoryEntities,
+        decoration: DBDecoration,
         canvas: HTMLCanvasElement, 
         group: IDynamicObject[],
         sides: number,
     ): AnimationInstance | null{
         try {
-            const cstr = SeedFactory.cstr[seed.__typename]
+            const cstr = SeedFactory.cstr[decoration.type]
             if (!cstr) return null
-            return new cstr(seed, canvas, group, sides)
+            return new cstr(decoration, canvas, group, sides)
+        } catch(e) {
+            return null
+        }
+    }
+
+    static getHydratedSeed(
+        decoration: DBDecoration,
+    ) {
+        try {
+            const cstr = SeedFactory.cstr[decoration.type]
+            if (!cstr) return null
+            return cstr.createObjectFromSeed(decoration)
+        } catch(e) {
+            return null
+        }
+    }
+
+    static isBackground(
+        category: DecorationType,
+    ) {
+        try {
+            const cstr = SeedFactory.cstr[category]
+            if (!cstr) return null
+            return cstr.isBackground
         } catch(e) {
             return null
         }
