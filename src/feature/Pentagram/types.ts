4196f2bc2222b4d77f7bfb82aaa0b891fbcf5f57
fauxRaccord888 <fauxRaccord888@gmail.com>
2024-04-29 00:40:22 +0900

    chore: 펜타그램 장식 관련 타이핑 추가

--- src/feature/Pentagram/types.ts
+++ src/feature/Pentagram/types.ts
@@ -1,18 +1,24 @@
 import type { DBMiniProfile } from "$feature/Profile/types";
 import type { ISlotRenderConfig } from "$feature/template/type";
-import type { PentagramNodesInfoFragment, PentagramRevisionsMinimalInfoFragment, PentagramsSelectInfoFragment, PentagramsUpdateInfoFragment } from "$lib/graphql/__generated__/graphql";
+import type { PentagramDecorationsMinimalInfoFragment, PentagramNodesInfoFragment, PentagramRevisionsMinimalInfoFragment, PentagramsSelectInfoFragment, PentagramsUpdateInfoFragment } from "$lib/graphql/__generated__/graphql";
 
 export type DBPentagram_SELECT = PentagramsSelectInfoFragment
 export type DBPentagram_UPDATE = PentagramsUpdateInfoFragment
 
 export type DBPentagramNodes = PentagramNodesInfoFragment
 export type DBPentagramRevisions = PentagramRevisionsMinimalInfoFragment
+export type DBPentagramDecorations = PentagramDecorationsMinimalInfoFragment
 
 export type PentagramSelectRenderConfig = ISlotRenderConfig<"metaInfo" | "mainPentagon" | "revision" | "player">
 export type PentagramSelectOptions = {
     displayRevisionIds?: string[] | null | undefined,
     forcedTimestamp?: Date | null | undefined
     enableAnimation?: boolean
+}
+
+export type PentagramDecorationPayload = {
+    pentagramId: string | undefined,
+    decorationId: string
 }
 
 export type PentagramEventHandler = {
